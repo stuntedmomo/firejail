7b31cd3812557e89bf96c9eaf12c66ed85e81bd6
startx2017 <vradu.startx@yandex.com>
2018-05-14 08:51:50 -0400

    clean old files in src/tools

--- src/tools/mkcoverit.sh
+++ src/tools/mkcoverit.sh
@@ -16,29 +16,6 @@ else
 	exit 1
 fi
 
-
-# unpack firetools archive
-ARCFIRETOOLS=`ls *.tar.bz2 | grep firetools`
-if [ "$?" -eq 0 ];
-then
-	echo "preparing $ARCFIRETOOLS"
-	DIRFIRETOOLS=`basename $ARCFIRETOOLS .tar.bz2`
-	rm -fr $DIRFIRETOOLS
-	tar -xjvf $ARCFIRETOOLS
-	cd $DIRFIRETOOLS
-	pwd
-	./configure --prefix=/usr
-	cd ..
-
-else
-	echo "Error: firetools source archive missing"
-	exit 1
-fi
-
-# move firetools in firejail source tree
-mkdir -p $DIRFIREJAIL/extras
-mv $DIRFIRETOOLS $DIRFIREJAIL/extras/firetools
-
 # build
 cd $DIRFIREJAIL
 cov-build --dir cov-int make -j 4 extras
