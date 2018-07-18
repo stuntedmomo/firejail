30a703f54930fe9212467956d49038fe4f5f74ee
startx2017 <vradu.startx@yandex.com>
2018-07-18 08:55:40 -0400

    monster mainline merge: whitelist hardening, whitelist symlink handling, fix whitelisting of /dev/log,  empty spaces in mountinfo fields

--- src/firejail/util.c
+++ src/firejail/util.c
@@ -996,91 +996,30 @@ void disable_file_path(const char *path, const char *file) {
 	free(fname);
 }
 
-#define MAX_BUF 4096
-static char mbuf[MAX_BUF];
-static MountData mdata;
-
-// Get info regarding the last kernel mount operation.
-// The return value points to a static area, and will be overwritten by subsequent calls.
-// The function does an exit(1) if anything goes wrong.
-MountData *get_last_mount(void) {
-	// open /proc/self/mountinfo
-	FILE *fp = fopen("/proc/self/mountinfo", "r");
-	if (!fp)
-		goto errexit;
-
-	mbuf[0] = '\0';
-	while (fgets(mbuf, MAX_BUF, fp));
-	fclose(fp);
-	if (arg_debug)
-		printf("%s", mbuf);
-
-	// extract filesystem name, directory and filesystem type
-	// examples:
-	//	587 543 8:1 /tmp /etc rw,relatime master:1 - ext4 /dev/sda1 rw,errors=remount-ro,data=ordered
-	//		mdata.fsname: /tmp
-	//		mdata.dir: /etc
-	//		mdata.fstype: ext4
-	//	585 564 0:76 / /home/netblue/.cache rw,nosuid,nodev - tmpfs tmpfs rw
-	//		mdata.fsname: /
-	//		mdata.dir: /home/netblue/.cache
-	//		mdata.fstype: tmpfs
-	memset(&mdata, 0, sizeof(mdata));
-	char *ptr = strtok(mbuf, " ");
-	if (!ptr)
-		goto errexit;
-
-	int cnt = 1;
-	while ((ptr = strtok(NULL, " ")) != NULL) {
-		cnt++;
-		if (cnt == 4)
-			mdata.fsname = ptr;
-		else if (cnt == 5) {
-			mdata.dir = ptr;
-			break;
-		}
-	}
-
-	ptr = strtok(NULL, "-");
-	if (!ptr)
-		goto errexit;
-
-	ptr = strtok(NULL, " ");
-	if (!ptr)
-		goto errexit;
-	mdata.fstype = ptr++;
-
-	if (mdata.fsname == NULL ||
-	    mdata.dir == NULL ||
-	    mdata.fstype == NULL)
-		goto errexit;
-	if (arg_debug)
-		printf("fsname=%s dir=%s fstype=%s\n", mdata.fsname, mdata.dir, mdata.fstype);
-	return &mdata;
-
-errexit:
-	fprintf(stderr, "Error: cannot read /proc/self/mountinfo\n");
-	exit(1);
-}
-
 // The returned file descriptor should be suitable for privileged operations on
 // user controlled paths. Passed flags are ignored if path is a top level directory.
 int safe_fd(const char *path, int flags) {
 	assert(path);
-	int fd;
+	int fd = -1;
 
 	// work with a copy of path
 	char *dup = strdup(path);
 	if (dup == NULL)
 		errExit("strdup");
-	if (*dup != '/')
-		errExit("relative path"); // or empty string
+	// reject relative path and empty string
+	if (*dup != '/') {
+		fprintf(stderr, "Error: invalid pathname: %s\n", path);
+		exit(1);
+	}
 
 	char *p = strrchr(dup, '/');
 	if (p == NULL)
 		errExit("strrchr");
-	if (*(p + 1) == '\0')
-		errExit("trailing slash"); // or root dir
+	// reject trailing slash and root dir
+	if (*(p + 1) == '\0') {
+		fprintf(stderr, "Error: invalid pathname: %s\n", path);
+		exit(1);
+	}
 
 	int parentfd = open("/", O_PATH|O_DIRECTORY|O_CLOEXEC);
 	if (parentfd == -1)
